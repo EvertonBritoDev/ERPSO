2cc6fcf4a4824271f4b28e1941f53fca236310f9
Everton Brito <everton.brito@koode.io>
2025-09-08 15:20:53 -0300

    updated/text

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,17 +4,16 @@ function Resume() {
   return (
     <div>
       <MenuPage />
-      <ResumePage></ResumePage>
+      
       <div className="fade-in">
+        <ResumePage></ResumePage>
         <Grid
           item
           xs={12}
           sm={4}
           sx={{ display: 'grid', height: '100vh', padding: '100px' }}
         >
-          <div>
-            
-          </div>
+          
         </Grid>
       </div>
     </div>
