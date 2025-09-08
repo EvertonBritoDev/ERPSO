163ca3ce9ee9423c4aae4a54a9108f07c5c9c57a
Everton Brito <everton.brito@koode.io>
2025-09-08 15:25:05 -0300

    updated/text

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,20 +1,12 @@
 import { MenuPage, ResumePage } from '@/components'
-import Grid from '@mui/material/Grid'
 function Resume() {
   return (
     <div>
       <MenuPage />
-      
+
       <div className="fade-in">
         <ResumePage></ResumePage>
-        <Grid
-          item
-          xs={12}
-          sm={4}
-          sx={{ display: 'grid', height: '100vh', padding: '100px' }}
-        >
-          
-        </Grid>
+        
       </div>
     </div>
   )
